96f030acca141464998bec09af42b14aea4622d4
sydney <lilyzxycu@163.com>
2024-08-24 17:29:04 +0800

    Implement RewardValue class and integrate it into RewardsConverter

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,16 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035;
+    }
+
+    public double getCashValue() {
+        return this.cashValue;
+    }
+    public double getMilesValue() {
+         return this.milesValue;
+    }
+}
